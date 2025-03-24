ead6c1b5e93cf58065a8671ff24f4b986c111474
Deiiivy <deivypr28@gmail.com>
2025-03-24 13:17:28 -0500

    adding class base Reserva.cs

---
+++ HabitacionReservas/Reserva.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitacionReservas
+{
+   public abstract class Reserva
+    {
+        public string NombreCliente { get; set; }
+        public int NumeroHabitacion { get; set; }
+
+        public DateTime FechaReserva { get; set; }
+        public int DuracionEstandia { get; set; }
+
+        public Reserva(string nombreCliente, int numeroHabitacion, DateTime fechaReserva, int duracionEstandia)
+        {
+            if(!string.IsNullOrEmpty(nombreCliente))
+            {
+                throw new ArgumentException("El nombre del cliente no puede estar vacio");
+            }
+            if (numeroHabitacion <= 0)
+            {
+                throw new ArgumentException("El numero de habitacion no puede ser menor o igual a 0");
+            }
+
+            NombreCliente = nombreCliente;
+            NumeroHabitacion = numeroHabitacion;
+            FechaReserva = fechaReserva;
+            DuracionEstandia = duracionEstandia;
+        }
+
+        public abstract double CalcularCosto();
+
+    }
+}
