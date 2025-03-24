6c9e41e9e76fa8aa836ab87836acae234a6963af
Deiiivy <deivypr28@gmail.com>
2025-03-24 13:29:32 -0500

    adding class HabitacionVIP.CS

---
+++ HabitacionReservas/HabitacionVIP.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitacionReservas
+{
+   public class HabitacionVIP : Reserva
+    {
+        public double TarifaFija { get; set; }
+        public const double Descuesto = 0.2;
+
+        public HabitacionVIP(string nombreCliente, int numeroHabitacion, DateTime fechaReserva,
+            int duracionEstandia, double tarifaFija)
+            : base(nombreCliente, numeroHabitacion, fechaReserva, duracionEstandia)
+        {
+            if (tarifaFija <= 0)
+            {
+                throw new ArgumentException("La tarifa fija no puede ser menor o igual a 0");
+            }
+            TarifaFija = tarifaFija;
+        }
+
+        public override double CalcularCosto()
+        {
+           double costoTotal = DuracionEstandia * TarifaFija;
+         
+            if(DuracionEstandia > 5)
+            {
+                costoTotal -= costoTotal * Descuesto;
+            }
+            return costoTotal;
+        }
+    }
+}
