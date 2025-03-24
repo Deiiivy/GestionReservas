cd8f98227af3c988ee8665ba159b54523e63c384
Deiiivy <deivypr28@gmail.com>
2025-03-24 13:10:02 -0500

    adding archives

---
+++ HabitacionReservas/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace HabitacionReservas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.NombreCliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NumeroHabitacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FechaReserva = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DuracionEstadia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CostoTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Escoja el tipo de habitacion";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Habitacion estandar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(432, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Habitacion VIP";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NombreCliente,
+            this.NumeroHabitacion,
+            this.FechaReserva,
+            this.DuracionEstadia,
+            this.CostoTotal});
+            this.dataGridView1.Location = new System.Drawing.Point(119, 85);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(547, 266);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // NombreCliente
+            // 
+            this.NombreCliente.HeaderText = "Nombre del cliente";
+            this.NombreCliente.Name = "NombreCliente";
+            // 
+            // NumeroHabitacion
+            // 
+            this.NumeroHabitacion.HeaderText = "Numero de habitacion";
+            this.NumeroHabitacion.Name = "NumeroHabitacion";
+            // 
+            // FechaReserva
+            // 
+            this.FechaReserva.HeaderText = "Fecha de reserva";
+            this.FechaReserva.Name = "FechaReserva";
+            // 
+            // DuracionEstadia
+            // 
+            this.DuracionEstadia.HeaderText = "Duracion de Estandia";
+            this.DuracionEstadia.Name = "DuracionEstadia";
+            // 
+            // CostoTotal
+            // 
+            this.CostoTotal.HeaderText = "Costo Total";
+            this.CostoTotal.Name = "CostoTotal";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreCliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NumeroHabitacion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FechaReserva;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DuracionEstadia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CostoTotal;
+    }
+}
+
