1e3c3527a5ec5737c3d335c7f2b8cd6b632cb3f1
Deiiivy <deivypr28@gmail.com>
2025-03-24 13:23:02 -0500

    adding class HabitacionEstandar.cs

--- HabitacionReservas/HabitacionEstandar.cs
+++ HabitacionReservas/HabitacionEstandar.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace HabitacionReservas
 {
-   public class HabitacionEstandar
+   public class HabitacionEstandar : Reserva
     {
+        public double TarifaFija { get; set; }
 
+        public HabitacionEstandar(string nombreCliente, int numeroHabitacion, DateTime fechaReserva, 
+            int duracionEstandia, double tarifaFija)
+            : base(nombreCliente, numeroHabitacion, fechaReserva, duracionEstandia)
+        {
+
+            if (tarifaFija <= 0)
+            {
+                throw new ArgumentException("La tarifa fija no puede ser menor o igual a 0");
+            }
+            TarifaFija = tarifaFija;
+        }
+
+        public override double CalcularCosto()
+        {
+            return DuracionEstandia * TarifaFija;
+        }
     }
 }
